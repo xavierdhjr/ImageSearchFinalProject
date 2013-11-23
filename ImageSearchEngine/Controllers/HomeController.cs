eead7eb5ffe636acf94a27cea5d3aa3958fb215c
xavier.hollis <durandh1@msu.edu>
2013-11-22 20:12:35 -0500

    Added OpenSurf to the actual project. Created a TrecDocIterator, added Lucene.NET to the project

--- ImageSearchEngine/Controllers/HomeController.cs
+++ ImageSearchEngine/Controllers/HomeController.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Lucene.Net.Index;
+using Lucene.Net.Analysis;
+using Lucene.Net.Store;
+using Lucene.Net.Documents;
+using Lucene.Net.Linq;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+
+using System.Collections;
+
 using KeypointExtraction;
+using ImageSearchEngine.Util;
 
 namespace ImageSearchEngine.Controllers
 {
     public class HomeController : Controller
     {
+        
         //
         // GET: /Home/
 
@@ -20,10 +32,47 @@ namespace ImageSearchEngine.Controllers
 
         }
 
+        const string INDEX_DIRECTORY = @"C:\Users\Raider\ImageSearchFinalProject\LuceneIndex\Index";
+        const string DOCUMENT_DIRECTORY = @"C:\Users\Raider\ImageSearchFinalProject\LuceneIndex\Collection\coll";
+
+        public ActionResult IndexDocuments()
+        {
+            DateTime start = DateTime.Now;
+
+            Analyzer analyzer = new StopAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            Directory dir = FSDirectory.Open(INDEX_DIRECTORY);
+            Directory docDirectory = FSDirectory.Open(DOCUMENT_DIRECTORY);
+            IndexWriter writer = new IndexWriter(dir, analyzer, true, IndexWriter.MaxFieldLength.UNLIMITED);
+
+            string[] files = docDirectory.ListAll();
+            foreach (string f in files)
+            {
+                TrecDocIterator iterator = new TrecDocIterator(DOCUMENT_DIRECTORY + "\\" + f);
+                Document d;
+                while (iterator.MoveNext())
+                {
+                    d = iterator.Current;
+                    if (d != null && d.GetField("contents") != null)
+                    {
+                        writer.AddDocument(d);
+                    }
+                }
+            }
+
+            writer.Dispose();
+
+            double finish = (DateTime.Now - start).TotalSeconds;
+            //TrecDocIterator iterator = new TrecDocIterator(
+
+            return View(model:"Index took " + finish + " seconds.");
+        }
+
         public ActionResult Index()
         {
+            
             return View();
         }
+
 
         public JsonResult Query(HttpPostedFileBase queryImage)
         {
---
+++ ImageSearchEngine/Util/TrecDocIterator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+using Lucene.Net.Documents;
+using Lucene.Net.Store;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace ImageSearchEngine.Util
+{
+    public class TrecDocIterator : IEnumerator<Document>
+    {
+        public Document Current
+        {
+            get
+            {
+                Document doc = new Document();
+                StreamReader reader = new StreamReader(_reader);
+                StringBuilder builder = new StringBuilder();
+                //StringReader stringReader;
+                try
+                {
+                    string line;
+                    Regex pattern = new Regex("<DOCNO>\\s*(\\S+)\\s*<");
+                    bool in_doc = false;
+                    while (true)
+                    {
+                        line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            _eof = true;
+                            break;
+                        }
+                        if (!in_doc)
+                        {
+
+                            if (line.StartsWith("<DOC>"))
+                                in_doc = true;
+                            else
+                                continue;
+                        }
+                        if (line.StartsWith("</DOC>"))
+                        {
+                            in_doc = false;
+                            builder.Append(line);
+                            break;
+                        }
+
+                        Match match = pattern.Match(line);
+                        if (match.Success)
+                        {
+                            string docno = match.Groups[1].Value;
+                            doc.Add(new Lucene.Net.Documents.Field("docno",docno,Field.Store.YES, Field.Index.NOT_ANALYZED));
+                        }
+                        builder.Append(line);
+                    }
+
+                    if(builder.Length > 0)
+                    {
+                        doc.Add(new Lucene.Net.Documents.Field("contents",builder.ToString(),Field.Store.NO, Field.Index.ANALYZED));
+                    }
+                }
+                catch (Exception e)
+                {
+                    doc = null;
+                }
+                return doc;
+            }
+        }
+
+        FileStream _reader;
+        string _filePath;
+        bool _eof;
+
+        public TrecDocIterator(string pathToFile)
+        {
+            _filePath = pathToFile;
+            Reset();
+        }
+
+        public bool MoveNext()
+        {
+            return !_eof;
+        }
+
+        public void Reset()
+        {
+            _eof = false;
+            _reader = File.OpenRead(_filePath);
+        }
+
+        public void Dispose()
+        {
+            _reader.Dispose();
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+}
